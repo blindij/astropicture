d818293bc102a631bb87722aa2e11388e84a5d01
Bjorn Lindi <blindij@users.noreply.github.com>
2020-03-11 13:29:48 +0000

    Add widget

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import {
 } from '@jupyterlab/application';
 
 import {
-	ICommandPalette
+	ICommandPalette, MainAreaWidget
 	} from '@jupyterlab/apputils';
 
+	import {
+	Widget
+	} from '@lumino/widgets';
 
 /**
  * Initialization data for the astropicture extension.
@@ -16,7 +19,31 @@ const extension: JupyterFrontEndPlugin<void> = {
   requires: [ICommandPalette],
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension astropicture is activated!');
-    console.log('ICommandPalette:', palette);
+
+    // Create a blank content widget inside of a MainAreaWidget
+    const content = new Widget();
+    const widget = new MainAreaWidget({content});
+    widget.id = 'apod-jupyterlab';
+    widget.title.label = 'Astronomy Picture';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string='apod:open';
+    app.commands.addCommand(command, {
+    	label: 'Random Astronomy Picture',
+        execute: () => {
+            if (!widget.isAttached){
+                // Attach the widget to the main work area if it's not there
+                app.shell.add(widget,'main');
+            } 
+            // Activate the widget
+            app.shell.activateById(widget.id);
+        }
+    });
+
+    // Add the command to the palette
+    palette.addItem({command, category: 'Tutorial'});
+    // console.log('ICommandPalette:', palette);
   }
 };
 
